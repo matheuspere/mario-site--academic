998b298d3ae966532b78b40f9208d7d7dd38cfd5
Matheus Pereira <128595942+matheuspere@users.noreply.github.com>
2024-05-15 09:41:46 -0300

    Atualizar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Login from './Components/Login/login';
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Components/Login/login.jsx
@@ -0,0 +1,14 @@
+import React from "react" ;
+
+const login = () => {
+  return (
+    <div className="container">
+        <form >
+            <h1>Acesse o Sistema</h1>
+            <input type="email" placeholder="Matricula" />
+        </form>
+    </div>
+  ) ;
+};
+
+export default Login;
