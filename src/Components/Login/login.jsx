d5ca0c34443c9402a99072abcc3f30d965afed61
Matheus Pereira <128595942+matheuspere@users.noreply.github.com>
2024-05-15 09:50:18 -0300

    Atualizado

--- src/Components/Login/login.jsx
+++ src/Components/Login/login.jsx
@@ -1,14 +1,62 @@
-import React from "react" ;
+import { useState } from "react";
+import { FaUser, FaLock } from "react-icons/fa";
+import "./Login.css";
 
-const login = () => {
+const Login = () => {
+  // Estados para armazenar as entradas do usuário
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Função que é chamada quando o formulário é enviado
+  const handleSubmit = (event) => {
+    // Impede que a página seja recarregada
+    event.preventDefault();
+
+    // Faz o console log das credenciais do usuário
+    console.log("Dados de Login:", { username, password });
+  };
+
   return (
     <div className="container">
-        <form >
-            <h1>Acesse o Sistema</h1>
-            <input type="email" placeholder="Matricula" />
-        </form>
+      <form onSubmit={handleSubmit}>
+        <h1>Acesse o sistema</h1>
+        <div className="input-field">
+          <input
+            type="text"
+            placeholder="E-mail"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FaUser className="icon" />
+        </div>
+        <div className="input-field">
+          <input
+            type="password"
+            placeholder="Senha"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FaLock className="icon" />
+        </div>
+
+        <div className="recall-forget">
+          <label>
+            <input type="checkbox" />
+            Lembre de mim
+          </label>
+          <a href="#">Esqueceu sua senha?</a>
+        </div>
+        <button type="submit">Login</button>
+        <div className="signup-link">
+          <p>
+            Não tem uma conta? <a href="#">Registar</a>{" "}
+          </p>
+        </div>
+      </form>
     </div>
-  ) ;
+  );
 };
 
 export default Login;
